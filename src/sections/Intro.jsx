d33f4a6ee198f605c029ae511e2e6a34c00168bb
catalinbroinas <catalinbroinas@yahoo.com>
2025-10-25 13:11:21 +0300

    style(Intro): update spacing on mobile devices

--- src/sections/Intro.jsx
+++ src/sections/Intro.jsx
@@ -3,9 +3,9 @@ function Intro() {
   return (
     <section className="intro shadow-2 mb-8">
       <div className="container-fluid text-center">
-        <h1 className="intro__title mb-3">Football players</h1>
+        <h1 className="intro__title mb-2 mb-md-3">Football players</h1>
         
-        <div className="d-flex justify-center align-center gap-md mb-3">
+        <div className="d-flex flex-center gap-2 gap-md-3 mb-2 mb-md-3">
           <hr className="intro__hr" />
           <h2 className="intro__subtitle mb-0">Season 2025-2026</h2>
           <hr className="intro__hr" />
