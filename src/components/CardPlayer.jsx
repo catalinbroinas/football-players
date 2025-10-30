84b229773c9c01bc2300172ba941c2d81d02ad7e
catalinbroinas <catalinbroinas@yahoo.com>
2025-10-30 10:00:27 +0200

    feat(CardPlayer): add card player component

---
+++ src/components/CardPlayer.jsx
@@ -0,0 +1,26 @@
+
+function CardPlayer({ image, name, dateOfBirth, position, team }) {
+  const age = 20;
+
+  return (
+    <div className="card card-player">
+      <div className="card-image">
+        <img src={image} alt={`Photo of ${name}`} className="img-fluid" />
+      </div>
+
+      <div className="card-header">
+        <h3 className="card-title">{name}</h3>
+        <h4 className="card-subtitle">{team}</h4>
+      </div>
+
+      <div className="card-body">
+        <div className="d-flex justify-content-between align-items-center">
+          <p className="card-text m-0">{age} years</p>
+          <p className="card-text m-0">{position}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CardPlayer;
