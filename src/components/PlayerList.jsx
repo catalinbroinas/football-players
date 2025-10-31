e3e6db538c47c3bdf4e058a8578197f35254554a
catalinbroinas <catalinbroinas@yahoo.com>
2025-10-31 11:10:03 +0200

    feat(data): add players data

--- src/components/PlayerList.jsx
+++ src/components/PlayerList.jsx
@@ -1,41 +1,7 @@
 import CardPlayer from "./CardPlayer";
+import players from "../data/players";
 
 function PlayerList() {
-  const players = [
-    {
-      id: 1,
-      name: 'Cristiano Ronaldo',
-      team: 'Al-Nassr',
-      position: 'Forward',
-      dateOfBirth: '1985-02-05',
-      image: 'https://images.pexels.com/photos/31067777/pexels-photo-31067777.jpeg',
-    },
-    {
-      id: 2,
-      name: 'Kylian Mbappé',
-      team: 'PSG',
-      position: 'Forward',
-      dateOfBirth: '1998-12-20',
-      image: 'https://images.pexels.com/photos/3621104/pexels-photo-3621104.jpeg',
-    },
-    {
-      id: 3,
-      name: 'Cristiano Ronaldo',
-      team: 'Al-Nassr',
-      position: 'Forward',
-      dateOfBirth: '1985-02-05',
-      image: 'https://images.pexels.com/photos/31067777/pexels-photo-31067777.jpeg',
-    },
-    {
-      id: 4,
-      name: 'Kylian Mbappé',
-      team: 'PSG',
-      position: 'Forward',
-      dateOfBirth: '1998-12-20',
-      image: 'https://images.pexels.com/photos/3621104/pexels-photo-3621104.jpeg',
-    },
-  ];
-
   return (
     <div className="player-list">
       {players.map(player => (
---
+++ src/data/players.js
@@ -0,0 +1,53 @@
+
+const players = [
+  {
+    id: crypto.randomUUID(),
+    name: 'James Maddison',
+    team: 'Tottenham',
+    position: 'Forward',
+    dateOfBirth: '1996-11-23',
+    image: 'https://images.pexels.com/photos/31067777/pexels-photo-31067777.jpeg',
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'Declan Rice',
+    team: 'Arsenal',
+    position: 'Midfielder',
+    dateOfBirth: '1999-01-14',
+    image: 'https://images.pexels.com/photos/3621104/pexels-photo-3621104.jpeg',
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'Cole Palmer',
+    team: 'Chelsea',
+    position: 'Midfielder',
+    dateOfBirth: '2002-06-05',
+    image: 'https://images.pexels.com/photos/31067777/pexels-photo-31067777.jpeg',
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'Erling Haaland',
+    team: 'Manchester City',
+    position: 'Forward',
+    dateOfBirth: '2000-07-21',
+    image: 'https://images.pexels.com/photos/3621104/pexels-photo-3621104.jpeg',
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'Bruno Fernandes',
+    team: 'Manchester Utd',
+    position: 'Midfielder',
+    dateOfBirth: '1994-08-09',
+    image: 'https://images.pexels.com/photos/31067777/pexels-photo-31067777.jpeg',
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'Mohamed Salah',
+    team: 'Liverpool',
+    position: 'Forward',
+    dateOfBirth: '1992-06-15',
+    image: 'https://images.pexels.com/photos/3621104/pexels-photo-3621104.jpeg',
+  },
+];
+
+export default players;
