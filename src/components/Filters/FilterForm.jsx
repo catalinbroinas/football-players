94fea1bdd3c636563184c0b1ec16729a0872658f
catalinbroinas <catalinbroinas@yahoo.com>
2025-11-01 12:19:30 +0200

    refactor(FilterForm): remove handleSubmit

--- src/components/Filters/FilterForm.jsx
+++ src/components/Filters/FilterForm.jsx
@@ -4,12 +4,8 @@ import players from "../../data/players";
 function FilterForm({ setSelectedTeam }) {
   const teams = [...new Set(players.map(player => player.team))].sort();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
-
   return (
-    <form className="form-container" onSubmit={handleSubmit}>
+    <form className="form-container">
       <SelectTeam teams={teams} setSelectedTeam={setSelectedTeam} />
     </form>
   );
