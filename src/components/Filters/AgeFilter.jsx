e387089747f267d3f6aa508b8d3a2a667e1702c6
catalinbroinas <catalinbroinas@yahoo.com>
2025-11-13 17:13:42 +0200

    feat(AgeFilter): improve layout and accessibility
    - Add <fieldset> and <legend> for grouped checkboxes
    - Add classes to prepare layout update

--- src/components/Filters/AgeFilter.jsx
+++ src/components/Filters/AgeFilter.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 function AgeFilter({ ageRange, onApply }) {
   const [localAgeRange, setLocalAgeRange] = useState({
@@ -11,10 +13,10 @@ function AgeFilter({ ageRange, onApply }) {
   }, [ageRange]);
 
   return (
-    <div className="form-row">
-      <p className="text fw-bold w-100 mb-0">Filter by age</p>
+    <fieldset className="form-fieldset">
+      <legend className="form-legend">Filter by age</legend>
 
-      <div className="form-column mb-2">
+      <div className="form-row filter-form__apply-ageRange">
         <div className="form-outline">
           <input
             type="number"
@@ -46,15 +48,15 @@ function AgeFilter({ ageRange, onApply }) {
             className="form-label"
           >Max age</label>
         </div>
-      </div>
 
-      <button
-        type="button"
-        className="btn-primary mx-auto"
-        onClick={() => onApply(localAgeRange)}
-        disabled={localAgeRange.min > localAgeRange.max}
-      >Apply</button>
-    </div>
+        <button
+          type="button"
+          className="btn-primary mx-auto"
+          onClick={() => onApply(localAgeRange)}
+          disabled={localAgeRange.min > localAgeRange.max}
+        >Apply</button>
+      </div>
+    </fieldset>
   );
 }
 
