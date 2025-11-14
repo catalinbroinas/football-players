0fbcc82689b11111b6f1cef0e4a01fc5bf816324
catalinbroinas <catalinbroinas@yahoo.com>
2025-11-14 10:54:59 +0200

    refactor(Intro): use semantic classes
    - Remove utilities classes

--- src/sections/Intro.jsx
+++ src/sections/Intro.jsx
@@ -3,15 +3,15 @@ function Intro() {
   return (
     <section className="intro shadow-2">
       <div className="container-fluid text-center">
-        <h1 className="intro__title mb-2 mb-md-3">Football players</h1>
+        <h1 className="intro__title">Football players</h1>
         
-        <div className="d-flex flex-center gap-2 gap-md-3 mb-2 mb-md-3">
+        <div className="intro__season-wrapper">
           <hr className="intro__hr" />
-          <h2 className="intro__subtitle mb-0">Season 2025-2026</h2>
+          <h2 className="intro__subtitle">Season 2025-2026</h2>
           <hr className="intro__hr" />
         </div>
 
-        <p className="intro__text mb-0">
+        <p className="intro__text">
           Team players from Premier League
         </p>
       </div>
