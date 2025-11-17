fd89e694a2bfdee1321b1b462187ee73ce434507
catalinbroinas <catalinbroinas@yahoo.com>
2025-11-17 16:39:42 +0200

    chore(images): convert to WEBP and resize to 440x330

--- src/data/images.js
+++ src/data/images.js
@@ -1,12 +1,12 @@
-import rice from '../assets/img/players/rice.jpg';
-import salah from '../assets/img/players/salah.jpg';
-import palmer from '../assets/img/players/palmer.jpg';
-import maddison from '../assets/img/players/maddison.jpg';
-import haaland from '../assets/img/players/haaland.jpg';
-import bruno from '../assets/img/players/brunoF.jpg';
-import emiliano from '../assets/img/players/emilianoM.jpg';
-import trippier from '../assets/img/players/trieppier.jpg';
-import nwaneri from '../assets/img/players/nwaneri.png';
+import rice from '../assets/img/players/rice.webp';
+import salah from '../assets/img/players/salah.webp';
+import palmer from '../assets/img/players/palmer.webp';
+import maddison from '../assets/img/players/maddison.webp';
+import haaland from '../assets/img/players/haaland.webp';
+import bruno from '../assets/img/players/brunoF.webp';
+import emiliano from '../assets/img/players/emilianoM.webp';
+import trippier from '../assets/img/players/trieppier.webp';
+import nwaneri from '../assets/img/players/nwaneri.webp';
 
 
 const images = {
